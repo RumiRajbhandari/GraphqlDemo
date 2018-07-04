55373b27db04d09876d8c8c36babd9cc285db501
rumi <rumi@Rumis-MacBook-Pro.local>
2018-07-04 17:42:42 +0545

    Student list added in recyclerview

--- app/src/main/java/com/example/rumi/apollodemo/MainActivity.kt
+++ app/src/main/java/com/example/rumi/apollodemo/MainActivity.kt
@@ -1,20 +1,27 @@
 package com.example.rumi.apollodemo
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
 import com.apollographql.apollo.api.Response
 import com.apollographql.apollo.rx2.Rx2Apollo
+import com.example.rumi.apollodemo.databinding.ActivityMainBinding
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.observers.DisposableSingleObserver
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_main)
         getStudents()
+
     }
 
     fun getStudents() {
@@ -24,8 +31,14 @@ class MainActivity : AppCompatActivity() {
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribeWith(object : DisposableSingleObserver<Response<AllStudentsQuery.Data>>() {
                     override fun onSuccess(t: Response<AllStudentsQuery.Data>) {
-                        textName.text=t.data()?.allStudents().toString()
                         println("onSuccess $t")
+                        val linearLayoutManager = LinearLayoutManager(this@MainActivity)
+                        linearLayoutManager.orientation = LinearLayoutManager.VERTICAL
+                        var studentAdapter=StudentAdapter(t.data()?.allStudents()?.toList())
+                        binding.recyclerView.apply {
+                            layoutManager = linearLayoutManager
+                            adapter=studentAdapter
+                        }
                     }
 
                     override fun onError(e: Throwable) {
---
+++ app/src/main/java/com/example/rumi/apollodemo/StudentAdapter.kt
@@ -0,0 +1,34 @@
+package com.example.rumi.apollodemo
+
+import android.databinding.DataBindingUtil
+import android.databinding.ViewDataBinding
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.ViewGroup
+
+class StudentAdapter(private val studentList: List<AllStudentsQuery.AllStudent>?): RecyclerView.Adapter<StudentAdapter.StudentViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StudentViewHolder {
+        val layoutInflater=LayoutInflater.from(parent.context)
+        val binding = DataBindingUtil.inflate<ViewDataBinding>(layoutInflater, R.layout.stuent_single_row,parent,false)
+        return StudentViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return studentList!!.size
+    }
+
+    override fun onBindViewHolder(holder: StudentViewHolder, position: Int) {
+        val student= studentList!![position]
+            holder.bind(student.name())
+
+
+    }
+
+    inner class StudentViewHolder(val binding:ViewDataBinding):RecyclerView.ViewHolder(binding.root){
+        fun bind(item:kotlin.Any){
+            binding.setVariable(BR.item, item)
+            binding.executePendingBindings()
+        }
+    }
+}
